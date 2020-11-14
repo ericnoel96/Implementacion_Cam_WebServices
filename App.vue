e165b7ae4285bfc7c187b8a021d552f224406230
ericnoel96 <eric.noel@udla.edu.ec>
2020-11-14 07:52:54 -0500

    impl con apk

---
+++ App.vue
@@ -0,0 +1,48 @@
+<template>
+  <app-navigator></app-navigator>
+</template>
+
+<script>
+import {
+  createAppContainer,
+  createMaterialTopTabNavigator,
+  createStackNavigator
+} from "vue-native-router";
+
+import HomeScreen from "./screens/Home.vue";
+import CameraScreen from "./screens/Camera.vue";
+import WebServiceScreen from "./screens/WebService.vue";
+
+
+const Tabs = createMaterialTopTabNavigator({
+  Camera: HomeScreen,
+  Random: WebServiceScreen,
+});
+
+const StackNavigator = createStackNavigator(
+  {
+    Tabs,
+    Camara: CameraScreen
+  },
+  {
+    defaultNavigationOptions: {
+      gestureEnabled: false,
+      title: "EA-NR - este si",
+      headerTitleStyle: {
+        textAlign: "center",
+        alignSelf: "center",
+        color: "white"
+      },
+      headerStyle: {
+        backgroundColor: "#d10502"
+      }
+    }
+  }
+);
+
+const AppNavigator = createAppContainer(StackNavigator);
+
+export default {
+  components: { AppNavigator }
+};
+</script>
